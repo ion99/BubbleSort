9c19fd2db86b3277114475cfbb1e3845f292b503
Ion Gorincioi <gorincioii@yahoo.com>
2017-10-21 15:29:58 -0500

    Update bubbleSort.js

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,11 +1,11 @@
-function bubbleSort (input) {
+const bubbleSort = input => {
 
-  for (var i=0; i<input.length-1; i++){
-    for (var j=i+1; j<input.length; j++){
+  for (let i=0; i<input.length-1; i++){
+    for (let j=i+1; j<input.length; j++){
       if (input[j] < input[i]) {
-        var temp = input[i];
-          input[i] = input[j];
-          input[j] = temp;
+        let temp = input[i];
+        input[i] = input[j];
+        input[j] = temp;
       } 
     }
   }
